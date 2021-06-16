c151836d1758bf122b8b4aed8af0dcd25689a7a2
Mantsali Nthabiseng Sekoli <mnsekoli@gmail.com>
2021-06-16 16:14:11 +0200

    form client side validation

--- assets/main.js
+++ assets/main.js
@@ -24,43 +24,87 @@ const navslide = () =>{
  
 }
 
-navslide();
-
-let contactForm = document.getElementById("contactForm");
-
-let name = document.getElementById("name");
-let email = document.getElementById("email");
-let message = document.getElementById("message");
+const sendEmail = () =>{
+  const contactForm = document.getElementById("contactForm");
 
 
-async function submitForm(url , data ){
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const message = document.getElementById("message");
+  const errEmail = document.querySelector(".email-err");
+  const errMessage = document.querySelector(".message-err");
+  const errName = document.querySelector(".name-err");
 
-  const response = await fetch(url, {
-    method: 'POST', headers: {
-      "content-type": "application/json"
+  async function submitForm(url , data ){
 
-    }, 
-    body: JSON.stringify(data)
-  });
+    await fetch(url, {
+      method: 'POST', 
+      body: data
+    }).then(response => response.text()).then(response => {
+      return response;
+    }).catch(error => {return error});
+
+  }
+  const resetElem = (elem) =>{
+    elem.classList.remove("invalid");
+  }
+  const validateEmail = () =>{
+
+    resetElem(email);
+    errEmail.innerText = '';
+
+    if(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.value)){
+      return true;
+    }
+    email.classList.add("invalid");
+    errEmail.innerText = "Email is invalid";
+    return false;
+  }
+
+  const validateInputs = () => {
+    resetElem(name);
+    resetElem(message);
+    errMessage.innerText = '';
+    errName.innerText = '';
+    
+    if(!name.value){
+      email.classList.add("invalid");
+      errName.innerText = "Please enter a name";
+      return false;
+    }
+    if(!name.value){
+      email.classList.add("invalid");
+      errMessage.innerText = "Please write your message";
+      return false;
+    }
+    
+
+    return true;
+  }
 
 
-  return response.json();
-}
-
-contactForm.addEventListener("submit", e => {
+  contactForm.addEventListener("submit", e => {
     e.preventDefault();
     //submitForm();
-      const data = new URLSearchParams();
+
+  if(validateEmail() && validateInputs()){
+
+    const data = new URLSearchParams();
 
     for(const p of new FormData(contactForm)){
       data.append(p[0], p[1]);
     }
 
-    fetch('send.php',{
-      method: 'POST',
-      body: data
-    }).then(response => response.text()).then(response => {
-      console.log(response);
-    }).catch(error => console.log(error));
-    //submitForm("test.php", {name: name.value});
+    
+    
+      document.querySelector('success-message').innerText = submitForm("send.php", data);
+    }
+  
 });
+
+
+}
+
+
+navslide();
+sendEmail();
